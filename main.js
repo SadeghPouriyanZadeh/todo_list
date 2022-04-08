69ab0bb009abfa5d4405d34fd848b6202436e4c5
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-04-08 18:23:40 +0430

    intial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+let btn = document.getElementById("addButton");
+let list = document.getElementById("todoList");
+let todoTextBox = document.getElementById("todoTextBox");
+
+function addItem(e) {
+  let content = todoTextBox.value;
+  if (content.length > 0){
+    let li = document.createElement("li");
+    li.innerHTML = content;
+    list.appendChild(li);
+    todoTextBox.value = "";
+  }
+}
+
+function handleKeyPress(e) {
+    if (e.keyCode === 13) {
+        addItem();
+    }
+}
+
+todoTextBox.addEventListener("keypress", handleKeyPress);
+btn.addEventListener("click", addItem);
