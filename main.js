724ebebc41be21c101ab07eaddb20c4b4df87860
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-04-08 21:20:34 +0430

    Navbar Updated remove logo, add themes

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+import bulmaCalendar from "/node_modules/bulma-extensions/bulma-calendar/dist/bulma-calendar.min.js";
+
 let btn = document.getElementById("addButton");
 let list = document.getElementById("todoList");
 let todoTextBox = document.getElementById("todoTextBox");
 
 function addItem(e) {
   let content = todoTextBox.value;
-  if (content.length > 0){
+  if (content.length > 0) {
     let li = document.createElement("li");
     li.innerHTML = content;
     list.appendChild(li);
@@ -13,9 +15,9 @@ function addItem(e) {
 }
 
 function handleKeyPress(e) {
-    if (e.keyCode === 13) {
-        addItem();
-    }
+  if (e.keyCode === 13) {
+    addItem();
+  }
 }
 
 todoTextBox.addEventListener("keypress", handleKeyPress);
